d3fa1f8b7eb376371f109952c45ed32aac00e511
Sahil-Dash <sahildash386@gmail.com>
2024-05-05 12:51:47 +0530

    some minor  modification applied

--- src/app/_api/fetchDocs.ts
+++ src/app/_api/fetchDocs.ts
@@ -6,7 +6,6 @@ import { PAGES } from '../_graphql/pages'
 import { PRODUCTS } from '../_graphql/products'
 import { GRAPHQL_API_URL } from './shared'
 import { payloadToken } from './token'
-import Categories from '../../payload/collections/Categories'
 import { CATEGORIES } from '../_graphql/categories'
 
 const queryMap = {
